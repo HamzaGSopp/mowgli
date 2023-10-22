a390cf3daa5c550052caa3a8c31316a918ccabfe
HamzaGSopp <104453337+HamzaGSopp@users.noreply.github.com>
2023-10-22 14:13:45 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    app: {
+        px: '+',
+        token: 'MTE2NTYyMTI2Mjk3OTE4MjcwNA.Glzs2q.pRzT3HbwNc6t_ODlZEZUdu_U-wquIpYbXq_tQ0',
+        owners: '1020626992414064681',
+        funny: '1020626992414064681',
+        color: 'red',
+        footer: 'by HamzaGSopp',
+        maxserver: '130',
+        maxVol: '150',
+        everyoneMention: false,
+        hostedBy: true,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+}
